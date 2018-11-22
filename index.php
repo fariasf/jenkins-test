72f43c14aa17cf0e5ffb788d3514ac03fcbe9a4e
Facundo Farias <contacto@facundofarias.com.ar>
2018-11-21 21:28:47 -0300

    Pass PHPCS, Fail PHPMD

--- index.php
+++ index.php
@@ -12,38 +12,52 @@
  * @link     https://github.com/fariasf/jenkins-test/
  */
 
-class foo {
-	public function foo() {
-		bar::sayhi();
-	}
+/**
+ * Foo class
+ *
+ * @category Irrelevant.
+ * @package  Jenkins_Tests.
+ * @author   Facundo Farias <contacto@facundofarias.com.ar>
+ * @license  WTFPL <http://www.wtfpl.net/>
+ * @link     https://github.com/fariasf/jenkins-test/
+ */
+Class Foo
+{
+    /**
+     * Foo the thing
+     */
+    public function foo()
+    {
+        // bar::sayhi();
+    }
 
-	public function bar() {
-		if ( true ) {
-			for( $i = 0; $i < 1000; $i++ ) {
-				if ( false ) {
-					if ( true ) {
-						$i--;
-					} else {
-						// Nothing
-					}
-				} else {
-					if ( $a = $b ) {
-						if ( $c ) {
-							continue;
-						}
-					}
-				}
-			}
-		} else {
-			if ( !!true ) {
-				// More stuff
-			}
-		}
-	}
-}
-
-static class bar {
-	public function sayhi() {
-		echo 'Hi';
-	}
+    /**
+     * Do bar
+     *
+     * @return void
+     */
+    public function bar()
+    {
+        if ( true ) {
+            for ( $i = 0; $i < 1000; $i++ ) {
+                if ( false ) {
+                    if ( true ) {
+                        $i--;
+                    } else {
+                        // Nothing
+                    }
+                } else {
+                    if ( $a = $b ) {
+                        if ( $c ) {
+                            continue;
+                        }
+                    }
+                }
+            }
+        } else {
+            if ( !!true ) {
+                // More stuff
+            }
+        }
+    }
 }
